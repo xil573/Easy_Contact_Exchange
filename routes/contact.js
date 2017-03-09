dbc822849a4de5b628bab175883779c8e05465c0
Xindi LI <xil573@eng.ucsd.edu>
2017-03-09 02:20:11 -0800

    styling

--- routes/contact.js
+++ routes/contact.js
@@ -1,15 +1,18 @@
 var data = require('../people.json');
 
 exports.view = function (req, res) {
-	//var id = req.params.id;
-	var id = req.params.id;
-	console.log("The contact id is: " + id);
+    //var id = req.params.id;
+    var id = req.params.id;
+    console.log("The contact id is: " + id);
 
-	for (i = 0; i < data.person.length; i++){
+    for (i = 0; i < data.person.length; i++){
       if (data.person[i].id == id){
-            
-            var contact = "Name : " + data.person[i].name + "\n" + "Phone : " + data.person[i].phone + "\n" + "Email : " + data.person[i].email;
-            console.log(contact);
+            var conName = data.person[i].name;
+            var conPhone = data.person[i].phone;
+            var conEmail = data.person[i].email;
+            var conImage = data.person[i].imageURL;
+            //var contact = "Name : " + data.person[i].name + "\n" + "Phone : " + data.person[i].phone + "\n" + "Email : " + data.person[i].email;
+            //console.log(contact);
    
         }
     }
@@ -17,6 +20,9 @@ exports.view = function (req, res) {
     //var contact = JSON.stringify(data);
     //console.log(string);
     res.render('contact', {
-    	'contactName': contact
+        'name': conName,
+        'phone': conPhone,
+        'email': conEmail,
+        'image': conImage
     });
 };
